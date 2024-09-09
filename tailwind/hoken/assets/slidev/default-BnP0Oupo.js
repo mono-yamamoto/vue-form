a71d91132404f30ce83264d2454506c783bea9b9
monosus_yamamoto <y.yamamoto@monosus.co.jp>
2024-09-09 22:18:09 +0900

    hoken

---
+++ tailwind/hoken/assets/slidev/default-BnP0Oupo.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-Dq3NkS3l.js";import{_ as r}from"../index-Dd7tYGnx.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
